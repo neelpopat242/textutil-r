472e9d9eecff037a2ca64027e973890751cdb1d5
Neel Popat <neelpopat242@gmail.com>
2021-12-25 08:46:13 +0530

    Updates

---
+++ precache-manifest.e768b95923a62e20616f33430d98bce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "30b66047cced50ce74e0",
+    "url": "/textutil-r/static/css/main.27d3e111.chunk.css"
+  },
+  {
+    "revision": "30b66047cced50ce74e0",
+    "url": "/textutil-r/static/js/main.30b66047.chunk.js"
+  },
+  {
+    "revision": "b30741ea70ee088bcae7",
+    "url": "/textutil-r/static/js/1.b30741ea.chunk.js"
+  },
+  {
+    "revision": "2a6980ee83e9e89aaf8a",
+    "url": "/textutil-r/static/js/2.2a6980ee.chunk.js"
+  },
+  {
+    "revision": "b5e49029e1a1c3df94fd",
+    "url": "/textutil-r/static/js/runtime~main.b5e49029.js"
+  },
+  {
+    "revision": "52ead8a9bb560f03c2221fa3a6313bc3",
+    "url": "/textutil-r/index.html"
+  }
+];
